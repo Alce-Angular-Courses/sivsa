b459c280ed9e0d0dfdb5ccd1827a707c307fe87c
alce65 <alejandro.cerezo@evosoft.es>
2021-08-30 18:16:03 +0200

    Recursos

---
+++ Recursos/libros.data.ts
@@ -0,0 +1,38 @@
+
+
+interface Libro {
+    id: number | string
+    titulo: string
+    autor: Array<string>
+    fecha: string
+}
+
+export const LIBROS: Array<Libro> = [
+    {
+        'id': 1,
+        "titulo": "Neuromante",
+        "autor": ["William Gibson"],
+        "fecha": "1984"
+    },
+    {
+        'id': 2,
+        "titulo": "Islas en la red",
+        "autor": ["Bruce Sterling"],
+        "fecha": "1988"
+    },
+    {
+        'id': 3,
+        "titulo": "Carbono Alterado",
+        "autor": ["Richard Morgan"],
+        "fecha": "2002"
+    },
+    {
+        "id": 4,
+        "titulo": "La máquina diferencial",
+        "autor": [
+            "Bruce Sterling",
+            "William Gibson"
+        ],
+        "fecha": "1991",
+    }
+];
